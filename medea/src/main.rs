53803d9a96c1659e7e92afd05f91fac24f323524
Noah <burghardtnoah@gmail.com>
2023-08-14 04:27:14 -0600

    updated workflow

--- medea/src/main.rs
+++ medea/src/main.rs
@@ -4,7 +4,7 @@ use colored::Colorize;
 
 fn main() {
     if let Err(err) = run() {
-        let error_str = "error:".red().bold();
+        let error_str = "error".red().bold();
         eprintln!("{}: {}", &error_str, err.to_string());
         std::process::exit(libc::EXIT_FAILURE);
     }
